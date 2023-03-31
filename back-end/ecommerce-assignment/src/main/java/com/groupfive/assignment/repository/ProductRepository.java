795f05d764cdf4e24033702f7351935be7f4a0a7
V-keerththanan <keerththananphy@gmail.com>
2023-03-31 08:58:52 +0530

    updated code

--- back-end/ecommerce-assignment/src/main/java/com/groupfive/assignment/repository/ProductRepository.java
+++ back-end/ecommerce-assignment/src/main/java/com/groupfive/assignment/repository/ProductRepository.java
@@ -3,9 +3,10 @@ package com.groupfive.assignment.repository;
 import com.groupfive.assignment._enum.ProductCategory;
 import com.groupfive.assignment.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByNameContainingIgnoreCase(String query);
     List<Product> findByCategory(ProductCategory category);
