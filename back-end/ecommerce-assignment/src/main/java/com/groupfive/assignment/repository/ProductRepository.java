ccd28cd3c80d3b4cafbefaacd02cc32917b5dd63
V-keerththanan <keerththananphy@gmail.com>
2023-04-07 14:28:37 +0530

    updated ProductRepository

--- back-end/ecommerce-assignment/src/main/java/com/groupfive/assignment/repository/ProductRepository.java
+++ back-end/ecommerce-assignment/src/main/java/com/groupfive/assignment/repository/ProductRepository.java
@@ -3,12 +3,16 @@ package com.groupfive.assignment.repository;
 import com.groupfive.assignment._enum.ProductCategory;
 import com.groupfive.assignment.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByNameContainingIgnoreCase(String query);
     List<Product> findByCategory(ProductCategory category);
+    @Query("SELECT p FROM Product p WHERE p.available = true ORDER BY p.id DESC")
+    List<Product> findLatestProducts(Pageable pageable);
     
 }
